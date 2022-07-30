c65ba065610d82e823619e13fad99c0165e71102
Lilly <lilly@lillycat.tech>
2022-07-30 23:48:51 +0100

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders text', () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Hello world/i);
+	const linkElement = screen.getByText(/Projects/i);
 	expect(linkElement).toBeInTheDocument();
 });
