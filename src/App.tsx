2354aa25263fbf7e77b68a373715b85a3a78d7b7
Lilly <lilly@lillycat.tech>
2022-07-30 21:10:31 +0100

    Fix the shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello world!</h1>
+      <Particles
+        params={{
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: "#000"
+            },
+            links: {
+              enable: true,
+              color: "#000",
+              distance: 150
+            },
+            move: {
+              enable: true
+            }
+          }
+        }}
+      />
+    </div >
   );
-}
+};
 
 export default App;
