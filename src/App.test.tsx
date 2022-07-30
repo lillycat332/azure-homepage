7c639efad0a8f9aaf225354d59103bf9702fe64d
Lilly <lilly@lillycat.tech>
2022-07-30 21:25:08 +0100

    Add tests to Appease CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders text', () => {
+	render(<App />);
+	const linkElement = screen.getByText(/Hello world/i);
+	expect(linkElement).toBeInTheDocument();
+});
